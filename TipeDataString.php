da81b929aeef0a85ef87ce0183e61593c552b232
Roland Salay <150308613+RolandSalay@users.noreply.github.com>
2023-11-28 16:59:46 +0900

    TipeDataString.php

--- TipeDataString.php
+++ TipeDataString.php
@@ -1,14 +1,14 @@
 <?php
 echo 'Name : ';
-echo 'Noel Ckristabel Maniawasi';
+echo 'Roland Imanuel Salay';
 echo "\n";
 
 echo "Name : ";
-echo "Noel\t ckristabel\t Maniawasi\n";
+echo "Roland\t Imanuel\t Salay\n";
 
-echo <<<Noel
+echo <<<Roland
 Selamat belajar PHP 
 sekarang, kita belajar tipe data string
 ono adalah cara ke-3 membuat string
 bisa menggunakan heredoc
-Noel;
+Roland;
